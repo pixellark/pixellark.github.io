b786feeec933a152785b39b948cc0cea83333df4
pixellark <136720900+pixellark@users.noreply.github.com>
2024-07-23 20:52:26 +0530

    fghjklkjhgv

---
+++ reviews-lk/js/header.js
@@ -0,0 +1,39 @@
+
+
+  // JavaScript code to add event listener on scroll
+  window.addEventListener("scroll", function () {
+    var header = document.querySelector(".headerdes"); // Changed the query selector to use .header1 class
+    if (window.scrollY >= 80) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+
+
+
+  const menuDiv = document.querySelector('.menu');
+  const menuSecDiv = document.querySelector('.menusec');
+
+
+  menuDiv.addEventListener('click', () => {
+    menuSecDiv.classList.remove('hiddenf');
+    var header = document.querySelector('.headerdes');
+    var scrollY = window.scrollY || window.pageYOffset;
+
+    header.style.position = 'absolute';
+    header.style.top = scrollY + 'px';
+  });
+
+
+  menuSecDiv.addEventListener('click', () => {
+    menuSecDiv.classList.add('hiddenf');
+    var header = document.querySelector('.headerdes');
+    function applyFixedPosition() {
+      header.style.position = 'fixed';
+      header.style.top = '0px';
+    }
+    const delay = 800; //link with menusec transition
+    setTimeout(applyFixedPosition, delay);
+  });
+
